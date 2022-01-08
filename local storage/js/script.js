fc3b7eabe6afeab0ead6fd4de3823809b18037b4
seymurquluzade <60833025+Seymur5080@users.noreply.github.com>
2022-01-08 16:09:39 +0400

    Add files via upload

---
+++ local storage/js/script.js
@@ -0,0 +1,41 @@
+// E60
+$('.img_flex div #e60').on('click', () => {
+   $('.img_flex div #e60').attr('src', 'img/e60.jpg').hide(1000).delay(1000).show(1000);
+   $('#div').show(1000).delay(1000).hide(1000);
+   $('#div img').attr('src', 'img/e60.jpg');
+});
+
+// F30
+$('.img_flex div #f30').on('click', () => {
+   $('.img_flex div #f30').attr('src', 'img/f30.jpg').fadeOut(1000).delay(1000).fadeIn(1000);
+   $('#div').fadeIn(1000).delay(1000).fadeOut(1000);
+   $('#div img').attr('src', 'img/f30.jpg');
+});
+
+// F10
+$('.img_flex div #f10').on('click', () => {
+   $('.img_flex div #f10').attr('src', 'img/f10.jpg').slideUp(1000).delay(1000).slideDown(1000);
+   $('#div').slideDown(1000).delay(1000).slideUp(1000);
+   $('#div img').attr('src', 'img/f10.jpg');
+});
+
+// E39
+$('.img_flex div #e39').on('click', () => {
+   $('.img_flex div #e39').attr('src', 'img/e39.jpg').hide(1000).delay(1000).show(1000);
+   $('#div').show(1000).delay(1000).hide(1000);
+   $('#div img').attr('src', 'img/e39.jpg');
+});
+
+// W204
+$('.img_flex div #w204').on('click', () => {
+   $('.img_flex div #w204').attr('src', 'img/w204.jpg').fadeOut(1000).delay(1000).fadeIn(1000);
+   $('#div').fadeIn(1000).delay(1000).fadeOut(1000);
+   $('#div img').attr('src', 'img/w204.jpg');
+});
+
+// E60-BLACK
+$('.img_flex div #e60-black').on('click', () => {
+   $('.img_flex div #e60-black').attr('src', 'img/e60-black.jpg').slideUp(1000).delay(1000).slideDown(1000);
+   $('#div').slideDown(1000).delay(1000).slideUp(1000);
+   $('#div img').attr('src', 'img/e60-black.jpg');
+});
